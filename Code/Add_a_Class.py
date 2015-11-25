3b05c23b9736860ef0592f08ea383fd48a3013a3
BergerKing <marcus.berger@mines.sdsmt.edu>
2015-11-24 17:12:19 -0700

    fixed add class and update teacher

--- Code/Add_a_Class.py
+++ Code/Add_a_Class.py
@@ -11,6 +11,13 @@ class add_Class(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.setup_database()
 
+        self.location_query = QSqlQuery()
+        self.location_query.exec_("SELECT DISTINCT Class_location FROM Class")
+        while self.location_query.next():
+            record = self.location_query.record()
+            self.location = str(record.value(0))
+            self.ui.Location_comboBox.addItem(self.location)
+
         self.ui.Submit_btn.clicked.connect(self.add_class)
         self.ui.Back_btn.clicked.connect(self.back)
         
@@ -18,13 +25,14 @@ class add_Class(QtGui.QMainWindow):
     def setup_database(self):
 
         self.conn() #need catch exception
-
         self.ui.sel_teach = QSqlRelationalTableModel(db = self.db)
         self.ui.sel_teach.setTable("Class")
 
         if not self.conn():
             QtGui.QMessageBox.warning(
                 self, 'Error', 'database contecting error')
+
+        
         
     def add_class(self):
         self.name = self.ui.Name_lineEdit.text()
@@ -32,7 +40,7 @@ class add_Class(QtGui.QMainWindow):
         self.time = self.ui.Time_timeEdit.time()
         self.time = self.time.toString("h:mm ap")
         self.date = str(self.ui.Day_comboBox.currentText())
-        self.location = self.ui.Location_lineEdit.text()
+        self.location = str(self.ui.Location_comboBox.currentText())
         self.cap = self.ui.Cap_spinBox.value()
         self.clothing = self.ui.Clothing_textEdit.toPlainText()    
         self.descirption = self.ui.Description_textEdit.toPlainText()
@@ -42,22 +50,42 @@ class add_Class(QtGui.QMainWindow):
         self.end = self.ui.End_date_dateEdit.date()
         self.end = self.end.toPyDate()
 
+        temp = "INSERT INTO Class %s VALUES( '%s','%s','%s','%s','%s','%s',\
+                        '%s','%s','%s','%s','%s'"
+        attributes = "(Class_id, Class_name, Class_cost, Class_time, Class_day, Class_location, Class_cap, Class_clothing, Class_description, Class_start_date, Class_end_date"
+
+
         self.age = self.ui.Age_spinBox.value()
+        if self.age != 0:
+            temp += ",'%d'" % self.age
+            attributes += ', Class_age'
         self.age_limit = self.ui.Age_limit_spinBox.value()
+        if self.age_limit != 0:
+            temp += ",'%d'" % self.age_limit
+            attributes += ', Class_age_end'
+        
+        attributes += ')'
+        temp += ')'
 
         id_query =QSqlQuery()
         id_query.exec_("SELECT Class_id FROM Class ORDER BY Class_id DESC LIMIT 1")
         while id_query.next():
             record = id_query.record()
-            self.id = str(record.value(0))
+            self.id = record.value(0)
+            self.id += 1
+            self.id = str(self.id)
             
-        
-        add_query = QSqlQuery()
-        add_query.exec_("INSERT INTO Class VALUES( %s,%s,%s,%s,%s,%s,\
-                        %s,%s,%s,%s,%s,%s,%s)", \
-                        (self.id, self.name, self.cost, self.time, self.date, self.location, \
-                           self.cap, self.clothing, self.descirption, self.start, self.end, \
-                           self.age, self.age_limit))
+        results_msg = "Are you sure you want to exit the program?"
+        reply = QtGui.QMessageBox.question(self, 'Message', 
+                     results_msg, QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            add_query = QSqlQuery()
+            add_query.exec_(temp \
+                            %(attributes, self.id, self.name, self.cost, self.time, self.date, self.location, \
+                               self.cap, self.clothing, self.descirption, self.start, self.end \
+                               ))
+        sys.exit()
     
 
     def back(self):
--- Code/Select_teacher_dialog.py
+++ Code/Select_teacher_dialog.py
@@ -1,59 +1,14 @@
 import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-from Select_Teacher import Select_teacher
-from Update_Teacher import modify_teacher
+from Update_Teacher import Ui_modify_teacher
 from PyQt4.QtSql import *
-
-class select_Teacher(QtGui.QDialog):
-    def __init__(self):
-        QtGui.QDialog.__init__(self)
-        self.ui = Select_teacher()
-        self.ui.setupUi(self)
-        self.setup_database()
-
-        self.ui.pushButton.clicked.connect(self.modify_information)
-
-        
-        
-    def setup_database(self):
-
-        
-        
-        self.conn() #need catch exception
-
-        self.ui.sel_teach = QSqlRelationalTableModel(db = self.db)
-        self.ui.sel_teach.setTable("Teacher")
-
-        if not self.conn():
-            QtGui.QMessageBox.warning(
-                self, 'Error', 'database contecting error')
-
-        query = QSqlQuery()
-        query.exec_("Select Teacher_name FROM Teacher")
-        while query.next():
-            record = query.record()
-            self.name = str(record.value(0))
-            self.ui.selectTeacherComboBox.addItem(self.name)
-        
-        
-    def modify_information(self):
-
-        self.Current_Window2 = modify_information()
-        
-        
-    def conn(self):
-        self.db = QSqlDatabase.addDatabase("QMYSQL")
-        self.db.setHostName("services1.mcs.sdsmt.edu")
-        self.db.setDatabaseName("db_dancesoft_f15")
-        self.db.setUserName("dancesoft_f15")
-        self.db.setPassword("DanceSoft")
-        return self.db.open()
+import re
 
 class modify_Information(QtGui.QDialog):
     def __init__(self):
         QtGui.QDialog.__init__(self)
-        self.modify = modify_teacher()
+        self.modify = Ui_modify_teacher()
         self.modify.setupUi(self)
 
         self.conn()
@@ -61,7 +16,26 @@ class modify_Information(QtGui.QDialog):
         self.modify.sel_teach = QSqlRelationalTableModel(db = self.db)
         self.modify.sel_teach.setTable("Teacher")
 
-        selected_teacher_name = "Jamie Witmore"
+        if not self.conn():
+            QtGui.QMessageBox.warning(
+                self, 'Error', 'database contecting error')
+
+        self.teach_query = QSqlQuery()
+        self.teach_query.exec_("Select Teacher_name FROM Teacher")
+        while self.teach_query.next():
+            record = self.teach_query.record()
+            self.name = str(record.value(0))
+            self.modify.selectTeacherComboBox.addItem(self.name)
+            
+        self.modify.Select_teacher_btn.clicked.connect(self.fill_form)
+
+        self.modify.Submit_btn.clicked.connect(self.submit_updates)
+
+    def fill_form(self):
+        self.modify.sel_teach = QSqlRelationalTableModel(db = self.db)
+        self.modify.sel_teach.setTable("Teacher")
+
+        selected_teacher_name = str(self.modify.selectTeacherComboBox.currentText())
         query = QSqlQuery()
         address_query =QSqlQuery()
         query.exec_("SELECT * FROM Teacher WHERE Teacher_name = '%s'" % selected_teacher_name)
@@ -89,13 +63,21 @@ class modify_Information(QtGui.QDialog):
             self.modify.payRateDoubleSpinBox.setValue(float(record.value(9)))
             self.modify.textEdit.setText(str(record.value(10)))
 
-        self.modify.pushButton.clicked.connect(self.submit_updates)
-
     def submit_updates(self):
         self.name = self.modify.nameLineEdit.text()
+        
         self.home = self.modify.homePhoneLineEdit.text()
+        self.home = re.sub('[^0-9]+', '', self.home)
+        self.home = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1-", "%d" % int(self.home[:-1])) + self.home[-1]
+        
         self.cell = self.modify.cellPhoneLineEdit.text()
+        self.cell = re.sub('[^0-9]+', '', self.cell)
+        self.cell = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1-", "%d" % int(self.cell[:-1])) + self.cell[-1]
+        
         self.work = self.modify.workPhoneLineEdit.text()
+        self.work = re.sub('[^0-9]+', '', self.work)
+        self.work = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1-", "%d" % int(self.work[:-1])) + self.work[-1]
+        
         self.address = self.modify.addressLineEdit.text()
         self.city = self.modify.cityLineEdit.text()
         self.state = str(self.modify.stateComboBox.currentText())    
@@ -105,20 +87,40 @@ class modify_Information(QtGui.QDialog):
         self.pay = self.modify.payRateDoubleSpinBox.value()
         self.medical = self.modify.textEdit.toPlainText()
 
-        update_query = QSqlQuery()
-        update_query.exec_("Update Teacher SET Teacher_name ='%s', \
-                            Teacher_home_phone ='%s', Teacher_cell_phone ='%s',\
-                            Teacher_work_phone ='%s', Teacher_email ='%s',\
-                            Teacher_sex ='%s', Teacher_SSN ='%s', \
-                            Teacher_pay_rate ='%s', Teacher_medical_information ='%s' WHERE \
-                            Teacher_id ='%s'" \
-                           %( self.name, self.home, self.cell, self.work, self.email, \
-                           self.gender, self.ssn, self.pay, self.medical, self.Teacher_id))
+        if self.name == '':
+            QtGui.QMessageBox.warning(
+                self, 'Error', "Please fill in required fields: Name, House Phone, Address, City, State, Gender, and Social security number(SSN).\
+                \nPhone number format: ###-###-#### \nAddress format: ### Street \nSNN format: ###-##-####" )
+            
+        elif self.home == '' or self.home.len() < 10:
+            QtGui.QMessageBox.warning(
+                self, 'Error', "Please fill in required fields: Name, House Phone, Address, City, State, Gender, and Social security number(SSN).\
+                \nPhone number format: ###-###-#### \nAddress format: ### Street \nSNN format: ###-##-####" )
+            
+        elif self.cell != '' and self.cell.len() < 10:
+            QtGui.QMessageBox.warning(
+                self, 'Error', "Please enter vaild phone number.\nPhone number format: ###-###-####" )
+            
+        elif self.work != '' and self.work.len() < 10:
+            QtGui.QMessageBox.warning(
+                self, 'Error', "Please enter vaild phone number.\nPhone number format: ###-###-####" )
+        
+            
+        else:
+            update_query = QSqlQuery()
+            update_query.exec_("Update Teacher SET Teacher_name ='%s', \
+                                Teacher_home_phone ='%s', Teacher_cell_phone ='%s',\
+                                Teacher_work_phone ='%s', Teacher_email ='%s',\
+                                Teacher_sex ='%s', Teacher_SSN ='%s', \
+                                Teacher_pay_rate ='%s', Teacher_medical_information ='%s' WHERE \
+                                Teacher_id ='%s'" \
+                               %( self.name, self.home, self.cell, self.work, self.email, \
+                               self.gender, self.ssn, self.pay, self.medical, self.Teacher_id))
 
-        update_query2 = QSqlQuery()
-        update_query2.exec_("Update Address SET Street ='%s', City ='%s',\
-                            State ='%s' WHERE Address_id ='%u'" %(self.address, \
-                            self.city, self.state, int(self.address_id)))
+            update_query2 = QSqlQuery()
+            update_query2.exec_("Update Address SET Street ='%s', City ='%s',\
+                                State ='%s' WHERE Address_id ='%u'" %(self.address, \
+                                self.city, self.state, int(self.address_id)))
 
         
 
